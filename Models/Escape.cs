1d514797c47b59eee68a0aed54f952cdb6d3e503
EITÁN GABRIEL FRIDMAN <48242472@ort>
2024-07-03 12:14:21 -0300

    commit 5

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -1,11 +1,11 @@
 public static class Escape
 {
-    private static string[] incognitasSalas = { "1979", "38-38", "2014", "2018", "50", "2021" };
+    private static string[] incognitasSalas = { "1979", "38-38", "y va el tercero", "50" };
     private static int estadoJuego = 1;
 
     private static void InicializarJuego()
     {
-        incognitasSalas = new string[] { "1979", "38-38", "2014", "2018", "50", "2021" };
+        incognitasSalas = new string[] { "1979", "38-38", "y va el tercero", "50" };
         estadoJuego = 1;
     }
 
--- obj/Debug/net8.0/TP05---FRIDMAN-DE-MARCO-WILDER.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP05---FRIDMAN-DE-MARCO-WILDER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP05---FRIDMAN-DE-MARCO-WILDER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ac98c220297cca563f38c0f007174fba08baa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b066d66938e179d65a75330c56152b38dfea71a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP05---FRIDMAN-DE-MARCO-WILDER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP05---FRIDMAN-DE-MARCO-WILDER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
